f57821c58d52827f54fa5f814a331aae9d4e4c2e
EvgeniaPatsoni <patsonievgenia@gmail.com>
2020-02-21 17:50:40 +0200

    Removed @ComponentScan

--- src/main/java/com/openwis/cicd/example/Main.java
+++ src/main/java/com/openwis/cicd/example/Main.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.openwis.helloworld.controller"})
 public class Main {
 
   public static void main(String[] args) {
