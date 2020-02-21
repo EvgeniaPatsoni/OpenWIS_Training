99b93aed4515d278811fb538e94858f45677731f
imousmoutis <imousmoutis@gmail.com>
2020-02-21 15:22:03 +0200

    Deploy script

--- remote_deploy.sh
+++ remote_deploy.sh
@@ -1,3 +1,15 @@
-pkill -9 -f cicd-example*
-nohup java -jar cicd-example-1.0.0-SNAPSHOT.jar &>/dev/null &
+#!/usr/bin/env bash
 
+set -xe
+
+ssh -o StrictHostKeyChecking=no "${DEPLOY_USER}"@"${DEPLOY_HOST}" << EOF
+echo "#################################################################################################################"
+
+	echo "Stop Spring Boot application execution";
+    pkill -9 -f cicd-example*
+
+	echo "Re-deploy Spring Boot application";
+	nohup java -jar cicd-example-1.0.0-SNAPSHOT.jar &>/dev/null &
+
+echo "#################################################################################################################"
+EOF
